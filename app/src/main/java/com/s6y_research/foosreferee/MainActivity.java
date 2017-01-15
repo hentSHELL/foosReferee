e7d89d74f15c0df122ed5a26261ffd14f81c1d35
hentSHELL <hentshell@gmail.com>
2017-01-15 01:50:19 +0100

    initial commit

---
+++ app/src/main/java/com/s6y_research/foosreferee/MainActivity.java
@@ -0,0 +1,17 @@
+package com.s6y_research.foosreferee;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView)findViewById(R.id.test);
+        textView.setText("Juhuu");
+    }
+}
